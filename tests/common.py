8b0a64b24b9315968f02ab350950fb716adaf0c6
Erez Shinan <erezshin+git@gmail.com>
2022-06-07 11:51:30 +0200

    Tiny fix

--- tests/common.py
+++ tests/common.py
@@ -8,6 +8,7 @@ logging.basicConfig(level=logging.WARN)
 TEST_MYSQL_CONN_STRING: str = "mysql://mysql:Password1@localhost/mysql"
 TEST_POSTGRES_CONN_STRING: str = None
 TEST_SNOWFLAKE_CONN_STRING: str = None
+TEST_BIGQUERY_CONN_STRING: str = None
 
 try:
     from .local_settings import *
