30cb696b9c750cb13fff8b1bc05204591697e4d9
Erez Shinan <erezshin+git@gmail.com>
2022-12-15 18:32:24 -0300

    main: Fix entrypoint name

--- sqeleton/__main__.py
+++ sqeleton/__main__.py
@@ -2,14 +2,14 @@ import click
 from .repl import repl as repl_main
 
 @click.group(no_args_is_help=True)
-def cli():
+def main():
     pass
 
-@cli.command(no_args_is_help=True)
+@main.command(no_args_is_help=True)
 @click.argument("database", required=True)
 def repl(database):
     return repl_main(database)
 
 
 if __name__ == '__main__':
-    cli()
+    main()
