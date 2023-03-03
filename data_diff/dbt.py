19cee875bb4836e524211994ee8cb070b42b164d
Dan <daniel@datafold.com>
2023-03-03 16:03:43 -0700

    fix merge conflict mistake

--- data_diff/dbt.py
+++ data_diff/dbt.py
@@ -361,6 +361,7 @@ class DbtParser:
             conn_info = {
                 "driver": conn_type,
                 "filepath": rendered_credentials.get("path"),
+            }
         elif conn_type == "redshift":
             if rendered_credentials.get("password") is None or rendered_credentials.get("method") == "iam":
                 raise Exception("Only password authentication is currently supported for Redshift.")
