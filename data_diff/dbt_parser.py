ef2e9195c8576f93b76281b316dee2013c74fc16
Dan <daniel@datafold.com>
2023-04-25 15:44:41 -0600

    add a dock link when vars do not exist

--- data_diff/dbt_parser.py
+++ data_diff/dbt_parser.py
@@ -63,8 +63,15 @@ class DbtParser:
         self.unique_columns = self.get_unique_columns()
 
     def get_datadiff_variables(self) -> dict:
-        vars = get_from_dict_with_raise(self.project_dict, "vars", f"No vars: found in dbt_project.yml.")
-        return get_from_dict_with_raise(vars, "data_diff", f"data_diff: section not found in dbt_project.yml vars:.")
+        doc_url = "https://docs.datafold.com/development_testing/open_source#configure-your-dbt-project"
+        vars = get_from_dict_with_raise(
+            self.project_dict, "vars", f"No vars: found in dbt_project.yml.\n\nConfigure your dbt project: \n{doc_url}\n"
+        )
+        return get_from_dict_with_raise(
+            vars,
+            "data_diff",
+            f"data_diff: section not found in dbt_project.yml.\n\nConfigure your dbt project: \n{doc_url}\n",
+        )
 
     def get_models(self):
         with open(self.project_dir / RUN_RESULTS_PATH) as run_results:
