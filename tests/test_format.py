9baf2569a8fc748b8d0355c15e4cb2c9a70de9cc
Valentin Khomutenko <valentine.khomutenko@gmail.com>
2023-07-06 18:41:57 +0200

    format

--- tests/test_format.py
+++ tests/test_format.py
@@ -106,56 +106,58 @@ class TestFormat(unittest.TestCase):
         )
 
     def test_jsonify_column_suffix_fix(self):
-            diff = DiffResultWrapper(
-                info_tree=InfoTree(
-                    info=SegmentInfo(
-                        tables=[
-                            TableSegment(table_path=("db", "schema", "table1"), key_columns=("id_a",), database=Database()),
-                            TableSegment(table_path=("db", "schema", "table2"), key_columns=("id_a",), database=Database()),
-                        ],
-                        diff_schema=(
-                            ("is_exclusive_a", bool),
-                            ("is_exclusive_b", bool),
-                            ("is_diff_id_a", int),
-                            ("is_diff_value_b", int),
-                            ("id_a_a", str),
-                            ("id_a_b", str),
-                            ("value_b_a", str),
-                            ("value_b_b", str),
-                        ),
-                        diff=[
-                            (False, False, 0, 1, "1", "1", "3", "201"),
-                            (True, False, 1, 1, "2", None, "4", None),
-                            (False, True, 1, 1, None, "3", None, "202"),
-                        ],
-                    )
-                ),
-                diff=[],
-                stats={},
-            )
-            json_diff = jsonify(diff, dbt_model="my_model")
-            self.assertEqual(
-                json_diff,
-                {
-                    "version": "1.0.0",
-                    "status": "success",
-                    "result": "different",
-                    "model": "my_model",
-                    "dataset1": ["db", "schema", "table1"],
-                    "dataset2": ["db", "schema", "table2"],
-                    "rows": {
-                        "exclusive": {
-                            "dataset1": [{"id_a": {"isPK": True, "value": "2"}, "value_b": {"isPK": False, "value": "4"}}],
-                            "dataset2": [{"id_a": {"isPK": True, "value": "3"}, "value_b": {"isPK": False, "value": "202"}}],
-                        },
-                        "diff": [
-                            {
-                                "id_a": {"isPK": True, "dataset1": "1", "dataset2": "1", "isDiff": False},
-                                "value_b": {"isPK": False, "dataset1": "3", "dataset2": "201", "isDiff": True},
-                            },
+        diff = DiffResultWrapper(
+            info_tree=InfoTree(
+                info=SegmentInfo(
+                    tables=[
+                        TableSegment(table_path=("db", "schema", "table1"), key_columns=("id_a",), database=Database()),
+                        TableSegment(table_path=("db", "schema", "table2"), key_columns=("id_a",), database=Database()),
+                    ],
+                    diff_schema=(
+                        ("is_exclusive_a", bool),
+                        ("is_exclusive_b", bool),
+                        ("is_diff_id_a", int),
+                        ("is_diff_value_b", int),
+                        ("id_a_a", str),
+                        ("id_a_b", str),
+                        ("value_b_a", str),
+                        ("value_b_b", str),
+                    ),
+                    diff=[
+                        (False, False, 0, 1, "1", "1", "3", "201"),
+                        (True, False, 1, 1, "2", None, "4", None),
+                        (False, True, 1, 1, None, "3", None, "202"),
+                    ],
+                )
+            ),
+            diff=[],
+            stats={},
+        )
+        json_diff = jsonify(diff, dbt_model="my_model")
+        self.assertEqual(
+            json_diff,
+            {
+                "version": "1.0.0",
+                "status": "success",
+                "result": "different",
+                "model": "my_model",
+                "dataset1": ["db", "schema", "table1"],
+                "dataset2": ["db", "schema", "table2"],
+                "rows": {
+                    "exclusive": {
+                        "dataset1": [{"id_a": {"isPK": True, "value": "2"}, "value_b": {"isPK": False, "value": "4"}}],
+                        "dataset2": [
+                            {"id_a": {"isPK": True, "value": "3"}, "value_b": {"isPK": False, "value": "202"}}
                         ],
                     },
-                    "summary": None,
-                    "columns": None,
+                    "diff": [
+                        {
+                            "id_a": {"isPK": True, "dataset1": "1", "dataset2": "1", "isDiff": False},
+                            "value_b": {"isPK": False, "dataset1": "3", "dataset2": "201", "isDiff": True},
+                        },
+                    ],
                 },
-            )
+                "summary": None,
+                "columns": None,
+            },
+        )
