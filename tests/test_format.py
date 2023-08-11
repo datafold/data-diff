63fff1c092ba65d3fd0d09c674da4f9877767a12
Dan <daniel@datafold.com>
2023-08-11 14:17:55 -0600

    add test case for --json --stats

--- tests/test_format.py
+++ tests/test_format.py
@@ -95,6 +95,83 @@ class TestFormat(unittest.TestCase):
             },
         )
 
+    def test_jsonify_no_stats(self):
+        diff = DiffResultWrapper(
+            info_tree=InfoTree(
+                info=SegmentInfo(
+                    tables=[
+                        TableSegment(table_path=("db", "schema", "table1"), key_columns=("id",), database=Database()),
+                        TableSegment(table_path=("db", "schema", "table2"), key_columns=("id",), database=Database()),
+                    ],
+                    diff_schema=(
+                        ("is_exclusive_a", bool),
+                        ("is_exclusive_b", bool),
+                        ("is_diff_id", int),
+                        ("is_diff_value", int),
+                        ("id_a", str),
+                        ("id_b", str),
+                        ("value_a", str),
+                        ("value_b", str),
+                    ),
+                    diff=[
+                        (False, False, 0, 1, "1", "1", "3", "201"),
+                        (True, False, 1, 1, "2", None, "4", None),
+                        (False, True, 1, 1, None, "3", None, "202"),
+                    ],
+                )
+            ),
+            diff=[],
+            stats={},
+        )
+        json_diff = jsonify(
+            diff,
+            dbt_model="my_model",
+            dataset1_columns=[
+                ("id", "NUMBER", Integer()),
+                ("value", "NUMBER", Integer()),
+            ],
+            dataset2_columns=[
+                ("id", "NUMBER", Integer()),
+                ("value", "NUMBER", Integer()),
+            ],
+            columns_diff={
+                "added": [],
+                "removed": [],
+                "typeChanged": [],
+            },
+            stats_only=True
+        )
+
+        self.assertEqual(
+            json_diff,
+            {
+                "version": "1.1.0",
+                "status": "success",
+                "result": "different",
+                "model": "my_model",
+                "dataset1": ["db", "schema", "table1"],
+                "dataset2": ["db", "schema", "table2"],
+                "rows": None,
+                "columns": {
+                    "dataset1": [
+                        {"name": "id", "type": "NUMBER", "kind": "integer"},
+                        {"name": "value", "type": "NUMBER", "kind": "integer"},
+                    ],
+                    "dataset2": [
+                        {"name": "id", "type": "NUMBER", "kind": "integer"},
+                        {"name": "value", "type": "NUMBER", "kind": "integer"},
+                    ],
+                    "primaryKey": ["id"],
+                    "exclusive": {
+                        "dataset1": [],
+                        "dataset2": [],
+                    },
+                    "typeChanged": [],
+                },
+                "summary": None,
+            },
+        )
+
     def test_jsonify_diff_no_difeference(self):
         diff = DiffResultWrapper(
             info_tree=InfoTree(
