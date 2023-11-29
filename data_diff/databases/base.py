933a393fa1877b36c21c517b121d970ef15320e1
Sarad Mohanan <sarad.mohanan@gmail.com>
2023-11-29 12:36:59 +0530

    minor
    Signed-off-by: Sarad Mohanan <sarad.mohanan@gmail.com>

--- data_diff/databases/base.py
+++ data_diff/databases/base.py
@@ -918,6 +918,9 @@ class Database(abc.ABC):
     is_closed: bool = False
     _dialect: BaseDialect = None
 
+    def __enter__(self):
+        return self
+
     def __exit__(self, exc_type, exc_value, traceback):
         self.close()
 
